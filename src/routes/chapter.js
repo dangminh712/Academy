e28ddf39e35c0a43b2a94f9de16ad702c13d2f59
minhnhox792 <minhnhox792@gmail.com>
2023-01-11 23:00:31 +0700

    Final UI, logic

--- src/routes/chapter.js
+++ src/routes/chapter.js
@@ -2,6 +2,7 @@
 import express from "express";
 import chapterController from "../app/controllers/chapterController.js"; 
 import upload from "../util/upload.js";
+import middleware from "../app/controllers/middleware/authMiddleware.js"
 
 
 
@@ -11,22 +12,22 @@ router.put("/:id", upload, chapterController.edit)
 
 // router.put('/:id', upload, chapterController.edit);
 
-router.delete('/overview/:id/:slug', chapterController.deleteOverview);
-router.delete('/basic/:id/:slug', chapterController.deleteBasic);
+router.delete('/overview/:id/:slug', middleware.isAuthenticated,chapterController.deleteOverview);
+router.delete('/basic/:id/:slug', middleware.isAuthenticated,chapterController.deleteBasic);
 router.delete('/master/:id/:slug', chapterController.deleteMaster);
 router.delete('/advanced/:id/:slug', chapterController.deleteAdvanced);
 
-router.get('/overview/:id', chapterController.allOverview);
-router.post('/overview/:id', upload, chapterController.addOverview);
+router.get('/overview/:id', middleware.isAuthenticated, chapterController.allOverview);
+router.post('/overview/:id', middleware.isAuthenticated, upload, chapterController.addOverview);
 
-router.get('/basic/:id', chapterController.allBasic);
-router.post('/basic/:id', upload, chapterController.addBasic);
+router.get('/basic/:id', middleware.isAuthenticated, chapterController.allBasic);
+router.post('/basic/:id', middleware.isAuthenticated, upload, chapterController.addBasic);
 
-router.get('/master/:id', chapterController.allMaster);
-router.post('/master/:id', upload, chapterController.addMaster);
+router.get('/master/:id', middleware.isAuthenticated, chapterController.allMaster);
+router.post('/master/:id', middleware.isAuthenticated, upload, chapterController.addMaster);
 
-router.get('/advanced/:id', chapterController.allAdvanced);
-router.post('/advanced/:id', upload, chapterController.addAdvanced); 
+router.get('/advanced/:id', middleware.isAuthenticated, chapterController.allAdvanced);
+router.post('/advanced/:id', middleware.isAuthenticated, upload, chapterController.addAdvanced); 
 
 
 router.get('/image/:filename', chapterController.imageRender);
--- src/routes/course.js
+++ src/routes/course.js
@@ -2,12 +2,13 @@ import express from 'express';
 import courseController from '../app/controllers/courseController.js';
 import upload from '../util/upload.js';
 const router = express.Router();
+import middleware from "../app/controllers/middleware/authMiddleware.js"
 
-router.get('/teacherAdd', courseController.teacherAdd);
-router.post('/teacherAdd', upload, courseController.postAdd);
-router.get('/edit/:id', courseController.edit);
-router.put('/edit/:id', upload, courseController.storeEdit);
-router.post('/comment', courseController.commentCourse);
+router.get('/teacherAdd',middleware.isAuthenticated, courseController.teacherAdd);
+router.post('/teacherAdd',middleware.isAuthenticated, upload, courseController.postAdd);
+router.get('/edit/:id',middleware.isAuthenticated, courseController.edit);
+router.put('/edit/:id',middleware.isAuthenticated, upload, courseController.storeEdit);
+router.post('/comment',middleware.isAuthenticated, courseController.commentCourse);
 router.get('/pathVideo', courseController.pathVideo);
 router.get('/about/:id', courseController.about);
 router.get('/:id', courseController.registerCourse);
